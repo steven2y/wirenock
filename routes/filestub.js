0c756f3654f8b85b1105c7d6850b6903409d1441
Steven Yang <steven.yang01.ext@bbc.co.uk>
2014-11-04 15:18:07 +0000

    Updated pattern matching

--- routes/filestub.js
+++ routes/filestub.js
@@ -9,28 +9,50 @@ var dir = '',
     mappings = [];
 
 
+function handleResponse(res, responseMatch) {
+    res.status(responseMatch.status)
+        .set(responseMatch.headers)
+        .sendFile(path.resolve(path.join(dir, '__files', responseMatch.bodyFileName)));
+}
 
-router.all('*', function(req, res, next) {
 
-    var match = _.filter(mappings, function (map) {
+router.all('*', function (req, res, next) {
+
+    //get the first match
+    var match = _.find(mappings, function (map) {
         return _.where([map.request], { url: req.originalUrl, method: req.method }).length > 0;
     });
 
-    if (match.length > 0) {
-        console.log('matched', req.originalUrl );
-        var response = match[0].response;
-        res.status(response.status)
-            .set(response.headers)
-            .sendFile(path.resolve(path.join(dir, '__files', response.bodyFileName)));
+    if (match === undefined) {
+        //if no match and there is a urlPattern then use that as regex against the incoming request
+        match = _.find(mappings, function (map) {
+            var mapRequest = map.request;
+            var result = false;
+
+            if (mapRequest.urlPattern) {
+                var reg = new RegExp(mapRequest.urlPattern);
+
+                if(req.method === mapRequest.method && reg.test(req.originalUrl)){
+                    result = true;
+                }
+            }
+            return result;
+        });
+
+    }
+
+    if (match !== undefined) {
+        console.log('matched', req.originalUrl, '-- against', match.request);
+        handleResponse(res, match.response);
     } else {
         next();
     }
 });
 
-module.exports = function(options){
+module.exports = function (options) {
     dir = options.dir;
     mappingsFile = fs.readdirSync(path.join(dir, 'mappings'));
-    mappingsFile.forEach(function(map){
+    mappingsFile.forEach(function (map) {
         mappings.push(require(path.resolve(path.join(dir, 'mappings', map))));
     });
     return router;
